cc69e0900689f1376c48ef9d390b0c0b74a92e39
saheba <mail-@-saheba-dot-net>
2014-07-23 20:01:10 +0200

    added rspec examples to test search by topic and bio

--- spec/controllers/search_spec.rb
+++ spec/controllers/search_spec.rb
@@ -4,8 +4,9 @@ describe SearchController do
 
   describe "search" do
     before do
-      @indexed_users =  create_array_of_users(['1ä','2ö','3ü','4ß'])
-      @unpublished_users = create_array_of_users(['1'], false,'unpub')
+      @indexed_users =  create_array_of_users(['1a','2b','3c','4d'])
+      @co_indexed_users =  create_array_of_users(['xx','yy','zz','ßß'], {common_topic: 'common äöüß', prefix: 'co'})
+      @unpublished_users = create_array_of_users(['1'], {published: false, prefix: 'unpub'})
     end
 
     context "when user is unublished" do
@@ -48,6 +49,38 @@ describe SearchController do
           end
         end
 
+        describe "user by unique topics" do
+          before do
+            get :search, q: @indexed_users[index].topics[0].name
+          end
+          it "finds only exact matches" do
+            ids = extract_ids_from_results(assigns(:results))
+            expect(ids.length).to eq 1
+            expect(ids).to include @indexed_users[index].id
+          end
+        end
+
+        describe "user by common topics (also covers downcase before search)" do
+          before do
+            get :search, q: @co_indexed_users[index].topics[0].name.upcase
+          end
+          it "finds all exact matches" do
+            ids = extract_ids_from_results(assigns(:results))
+            expect(ids.length).to eq @co_indexed_users.length
+            expect(ids).to include @co_indexed_users[index].id
+          end
+        end
+
+      describe "user by unique bio element" do
+        before do
+          get :search, q: @indexed_users[index].firstname.gsub('first name','')+'-wanga'
+        end
+        it "finds only exact matches" do
+          ids = extract_ids_from_results(assigns(:results))
+          expect(ids.length).to eq 1
+          expect(ids).to include @indexed_users[index].id
+        end
+      end
     # end of array
     end
   end
--- spec/support/array_helper.rb
+++ spec/support/array_helper.rb
@@ -5,15 +5,28 @@ module ArrayHelper
     return ids
   end
 
-  def create_array_of_users(ids, published=true, prefix='id')
+  def create_array_of_users(ids, options = { published: true, prefix: 'id', common_topic: nil })
+    options[:published] = true if options[:published] == nil
+    options[:prefix] = 'id' if options[:prefix] == nil
+
     users = []
     ids.each do |id|
+
       user = FactoryGirl.create(:published,
-        email: "#{prefix}#{id}@example.com",
-        firstname: "#{prefix}#{id}firstname",
-        lastname: "#{prefix}#{id}lastname",
-        published: published
+        email: "#{options[:prefix]}#{id}@example.com",
+        firstname: "#{options[:prefix]}#{id}first name",
+        lastname: "#{options[:prefix]}#{id}last name",
+        published: options[:published]
+
       )
+      if options[:common_topic] != nil
+        user.topic_list = options[:common_topic]
+      else
+        user.topic_list = "#{options[:prefix]}#{id}topic"
+      end
+
+      user.translation.bio = "#{options[:prefix]}#{id} is interested in teaching #{options[:prefix]}#{id}-wanga"
+
       user.save!
       users << user
     end
